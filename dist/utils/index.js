d071f69a60c27a9d2a3f67a5c33e61fb36d97bf1
gratenes <69170374+Gratenes@users.noreply.github.com>
2025-01-30 01:43:01 -0500

    support facebook

--- dist/utils/index.js
+++ dist/utils/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Utils = void 0;
 class Utils {
     checkForSocialMediaContent(input) {
-        const regex = /(tiktok|instagram|twitter|x\.com|snapchat|reddit|ifunny|youtu\w*|imgur|bsky)/gi;
+        const regex = /(tiktok|instagram|twitter|x\.com|snapchat|reddit|ifunny|youtu\w*|imgur|bsky|facebook|threads)/gi;
         const check = input.match(regex);
         return check !== null;
     }
